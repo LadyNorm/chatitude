c113152a1e2b08ad094b97fe8167c184f7b2b223
Nora Neyland <noraleeneyland@gmail.com>
2014-12-16 15:20:53 -0600

    edit

--- server.rb
+++ server.rb
@@ -1,6 +1,8 @@
 require 'pg'
 require 'sinatra'
 require 'json'
+require 'sinatra/reloader'
+require 'securerandom'
 
 require_relative 'lib/chatitude.rb'
 
@@ -19,4 +21,20 @@ post '/signup' do
   # localStorage.setItem('apiKey', api_key)
   # redirect to '/'
   return { apiToken: api_token }
+end
+
+post '/signin' do
+  db = Chat::DB.create_db_connection
+  username = params[:username]
+  password = params[:password]
+  api_token = Chat::DB.find_api_key(users['id'], db)
+  return { apiToken: api_token }
+end
+
+get '/chats' do
+  db = Chat::DB.create_db_connection
+end
+
+post '/chats' do
+  db = Chat::DB.create_db_connection
 end
